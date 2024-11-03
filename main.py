f0a4e0c87c45a45e6c9af15e5c801c760a142344
Fluke_Chatroj <67070218@kmitl.ac.th>
2024-11-04 04:57:47 +0700

    Add notic func,Temp unit,Thai text

--- main.py
+++ main.py
@@ -8,32 +8,42 @@ botid = "@736nacpz"
 channel_secret = "f386a079fe2b14019432506b89fb2864"
 line_access_token = "QVRlvdQy26KdOT8M0U9H6uY3jKqDfEAcO/3/7zL393n0B5mXOe14yr2/mhd9zIyzIeZwmEcv/2rhPZZkWhI6qm7YRFoz1difkx4POebgA+koA0quBBGHYUYr1mSopo/61pNIduEFDdGyRgx7gHIQwgdB04t89/1O/w1cDnyilFU="
 
-def get_weather(city):
+def umbrella(weather):
+    """ดูว่าต้องพกร่มหรือไม่"""
+    if weather in ("Rain", "Thunderstorm", "Drizzle"):
+        return "พกร่มด้วย!"
+    return ""
+
+def get_weather(city): #ไปดู metaweather.com
     """ดึงข้อมูลอากาศของจังหวัด"""
     web_api = "33486d92c9ef8a538e08fb13351385e6" # api key from openweatherapi
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={web_api}"
-    req = requests.get(url).json()
-
-    temp = req["main"]["temp"] #get temperature
-    return f"{city}'s temp = {temp}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={web_api}&units=metric&lang=th"
+    weather_info = requests.get(url).json()
+    
+    print(weather_info)
+    status = weather_info["weather"][0]["main"] #สภาพอากาศ
+    description = weather_info["weather"][0]["description"] #รายละเอียดอากาศ
+    noti = umbrella(status) #เตือนพกร่ม
+    #temp = weather_info["main"]["temp"] #อุณหภูมิ
+    return f"{city}'s status = {status}\ndescription = {description}\n{noti}"
 
 @app.route("/webhook", methods=['POST','GET'])
 def webhook():
     """Webhook"""
     if request.method == "POST":
         body = request.json
-        print(body)
-        replyToken = body['events'][0]['replyToken']
+        reply_token = body['events'][0]['replyToken']
         text = body['events'][0]['message']['text']
         weather = get_weather(text)
-        print(weather)
-        reply_message(replyToken, weather)
+        print(body) #check
+        print(weather) #check
+        reply_message(reply_token, weather)
         return request.json, 200
     if request.method == "GET":
         return 200
     abort(400)
 
-def reply_message(replyToken, message):
+def reply_message(reply_token, message):
     """ส่ง text reply user"""
     line_api = "https://api.line.me/v2/bot/message/reply"
     auth = f"Bearer {line_access_token}"
@@ -43,7 +53,7 @@ def reply_message(replyToken, message):
         "Authorization" : auth
     }
     data = {
-        "replyToken" : replyToken,
+        "replyToken" : reply_token,
         "messages":[{
             "type":"text",
             "text": message
